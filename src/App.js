165de981c60e00a73c90ca164de008e91e6ff7b4
manojkumar5252 <158422812+manojkumar5252@users.noreply.github.com>
2024-11-15 11:17:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CountryInfo from './CountryInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <CountryInfo/>
+      
+    </div>
+  );
+}
+
+export default App;
